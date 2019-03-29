c751387dcfe5ce5af9e151fdeab9b972a1167c56
cellularmatt <48768270+cellularmatt@users.noreply.github.com>
2019-03-29 10:30:12 -0700

    Update main.py
    Added time/date output for log

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ import get_xml as get
 import edit_xml as edit
 import convert_xml as convert
 import upload_xml as upload
+import time
+from datetime import datetime
+
+now = datetime.now()
+month = now.month
+day = now.day
+year = now.year
+hour = now.hour
+minute = now.minute
+second = now.second
+micsec = now.microsecond
+
+print('Started script on: '+ str(month) + '/'+ str(day) + '/'+ str(year) + ' '+ str(hour) + ':'+ str(minute) + ':'+ str(second) + ':' + str(micsec) )
+
+start = time.time()
 
 get.get_xml()
 edit.edit_xml()
 convert.convert_xml()
 upload.upload_xml()
+
+end = time.time()
+print('Script took ' + str(end - start) + ' seconds to run!\n\n')
